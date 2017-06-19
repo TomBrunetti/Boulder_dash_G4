3cb4420ca3b0fe63009d4e6e4195f6bb7802cc52
eliatib <thibault.michel@viacesi.fr>
2017-06-19 13:25:48 +0200

    contract

---
+++ BoulderDash_Base/BoulderDash/contract/src/main/java/Timer.java
@@ -0,0 +1,16 @@
+
+
+public class Timer {
+
+	private int time;
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+	public void timeDecrease(){}
+}
