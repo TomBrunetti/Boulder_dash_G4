bb51378c03cabe2db5f286a535d3aac5f0068961
eliatib <thibault.michel@viacesi.fr>
2017-06-20 11:55:14 +0200

    window test

--- BoulderDash_Base/BoulderDash/view/src/main/java/view/Window.java
+++ BoulderDash_Base/BoulderDash/view/src/main/java/view/Window.java
@@ -1,11 +1,16 @@
 package view;
+
 import javax.swing.JFrame;
 
 
 public class Window extends JFrame {
 
-	public Window(Map MyMap)
+	public void Window()
 	{
-		
+	    this.setTitle("BoulderDash 2.0");
+	    this.setSize(656, 656);
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.setLocationRelativeTo(null);
+	    this.setVisible(true);
 	}
 }
