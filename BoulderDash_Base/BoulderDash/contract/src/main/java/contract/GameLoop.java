292058ecde2bd11a96aadee30af91edd40ef2374
amasalski <antoine.masalski@viacesi.fr>
2017-06-22 09:45:03 +0200

    useless main and new event

--- BoulderDash_Base/BoulderDash/contract/src/main/java/contract/GameLoop.java
+++ BoulderDash_Base/BoulderDash/contract/src/main/java/contract/GameLoop.java
@@ -3,11 +3,6 @@ package contract;
 
 public class GameLoop {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 	public static void createGame(){}
 	
 	public static void endGame(){}
