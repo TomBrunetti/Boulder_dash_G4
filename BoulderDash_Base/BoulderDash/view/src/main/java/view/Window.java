6d3480ea59e0be78dedb3b95e09854fc2b3bc3ea
GeoffroyF <GeoffroyF@users.noreply.github.com>
2017-06-25 17:21:02 +0200

    Add files via upload

--- BoulderDash_Base/BoulderDash/view/src/main/java/view/Window.java
+++ BoulderDash_Base/BoulderDash/view/src/main/java/view/Window.java
@@ -1,4 +1,6 @@
 package view;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -10,21 +12,48 @@ import model.Entity;
 
 
 
-public class Window extends JFrame {
+public class Window extends JFrame implements KeyListener {
 	
+	int key=0;
+	private WindowElements we;
 	public void Window(ArrayList<Entity> entities)
 	{
 		JFrame win = new JFrame();
+		we = new WindowElements(entities);
 		DetectionTouches l = new DetectionTouches();
 	    win.setTitle("BoulderDash 2.0");
 	    win.setSize(246, 275);
 	    win.setResizable(false);
 	    win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    win.setLocationRelativeTo(null);
-	    win.add(new WindowElements(entities));
+	    win.add(we);
 	    win.setVisible(true);
-	    win.addKeyListener(l);
+	    win.addKeyListener(this);
 	    win.setFocusable(true);
+	}
+	public int getkey(){
+		//System.out.println(key);
+		return key;
+	}
+	public WindowElements getWe(){
+		return we;
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		 key = e.getKeyCode();
+		 System.out.println(key);
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		key=0;
+		System.out.println(key);
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 
